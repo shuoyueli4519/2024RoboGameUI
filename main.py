139b2d2c754c21ea3e23eaab5b90781a9ea67a8d
zhengyang <2621862968@qq.com>
2024-09-24 20:25:03 +0800

    提交美化后的UI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QLinearGradient, QPainter, QPixmap, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QLabel, QPushButton, QFileDialog
+from PyQt5.QtGui import QLinearGradient, QPainter, QPixmap, QColor, QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QLabel, QPushButton, QFileDialog, QStackedLayout
 
 class MainWindow(QMainWindow):
     def InitUI(self):
@@ -19,6 +19,8 @@ class MainWindow(QMainWindow):
         self.main_timer = QTimer(self)
         self.main_timer.timeout.connect(self.update)
         self.main_time_left = 420
+        self.fangdai = 420
+        self.fangdai_lock = 0
         self.red_team_flag = 0
         self.blue_team_flag = 0
         
@@ -29,110 +31,117 @@ class MainWindow(QMainWindow):
                                                     font-family:Microsoft YaHei; \
                                                     color: white;}")
         self.main_timer_label.setAlignment(Qt.AlignCenter)
-        self.main_timer_label.setGeometry(int(self.screen_width * 0.15), int(self.screen_height * 0.2),
+        self.main_timer_label.setGeometry(int(self.screen_width * 0.15), int(self.screen_height * 0.25),
                                           int(self.screen_width * 0.7), int(self.screen_height * 0.4))
         
         self.start_game = QPushButton(self)
-        self.start_game.setText("开始比赛")
-        self.start_game.setStyleSheet(" QPushButton{font-size:" + str(int(self.screen_width/1920 * 10 + self.screen_height/1080 * 10)) + "px; \
-                                        font-weight:bold; \
-                                        font-family:Microsoft YaHei; \
-                                        color: white; \
-                                        border: 2px solid white; \
+        self.start_game.setGeometry(int(self.screen_width * 0.3), int(self.screen_height * 0.65),
+                                      int(self.screen_width * 0.16), int(self.screen_height * 0.08))
+        icon = QIcon("assets/start.png")
+        self.start_game.setIcon(icon)
+        self.start_game.setIconSize(self.start_game.size())
+        self.start_game.setStyleSheet("QPushButton{border: 0px solid white; \
                                         border-radius: 10px;}")
-        self.start_game.setGeometry(int(self.screen_width * 0.38), int(self.screen_height * 0.7),
-                                      int(self.screen_width * 0.1), int(self.screen_height * 0.05))
         self.start_game.clicked.connect(self.start_game_clicked)
         
         self.reset = QPushButton(self)
-        self.reset.setText("复位")
-        self.reset.setStyleSheet("  QPushButton{font-size:" + str(int(self.screen_width/1920 * 10 + self.screen_height/1080 * 10)) + "px; \
-                                    font-weight:bold; \
-                                    font-family:Microsoft YaHei; \
-                                    color: white; \
-                                    border: 2px solid white; \
-                                    border-radius: 10px;}")
-        self.reset.setGeometry(int(self.screen_width * 0.52), int(self.screen_height * 0.7),
-                                int(self.screen_width * 0.1), int(self.screen_height * 0.05))
+        self.reset.setGeometry(int(self.screen_width * 0.42), int(self.screen_height * 0.65),
+                                int(self.screen_width * 0.16), int(self.screen_height * 0.08))
+        pixmap = QPixmap("assets/reset.png")
+        icon = QIcon(pixmap)
+        self.reset.setIcon(icon)
+        self.reset.setIconSize(self.reset.size())
+        self.reset.setStyleSheet("QPushButton{border: 0px solid white; \
+                                        border-radius: 10px;}")
         self.reset.clicked.connect(self.reset_clicked)
         
         self.game_status = QLabel(self)
-        self.game_status.setText("等待比赛中...")
-        self.game_status.setStyleSheet("QLabel{font-size:" + str(int(self.screen_width/1920 * 40 + self.screen_height/1080 * 40)) + "px; \
-                                        font-weight:bold; \
-                                        font-family:Microsoft YaHei; \
-                                        color: white;}")
-        self.game_status.setAlignment(Qt.AlignCenter)
-        self.game_status.setGeometry(int(self.screen_width * 0.35), int(self.screen_height * 0.15),
-                                    int(self.screen_width * 0.3), int(self.screen_height * 0.1))
+        self.game_status.setGeometry(int(self.screen_width * 0.3), int(self.screen_height * 0.18),
+                                    int(self.screen_width * 0.3), int(self.screen_height * 0.18))
+        pixmap = QPixmap("assets/waitgame.png")
+        self.game_status.setPixmap(pixmap)
+        self.game_status.setScaledContents(True)
         
         self.red_team = QPushButton(self)
-        self.red_team.setText("红方")
-        self.red_team.setStyleSheet("   QPushButton{font-size:" + str(int(self.screen_width/1920 * 40 + self.screen_height/1080 * 40)) + "px; \
-                                        font-weight:bold; \
-                                        font-family:Microsoft YaHei; \
-                                        border-radius: 10px; \
-                                        color: red;}")
-        self.red_team.setGeometry(int(self.screen_width * 0.08), int(self.screen_height * 0.2),
-                                    int(self.screen_width * 0.15), int(self.screen_height * 0.1))
+        self.red_team.setGeometry(int(self.screen_width * 0.45), int(self.screen_height * 0),
+                                    int(self.screen_width * 0.5), int(self.screen_height * 0.3))
+        pixmap = QPixmap("assets/redteam.png")
+        icon = QIcon(pixmap)
+        self.red_team.setIcon(icon)
+        self.red_team.setIconSize(self.red_team.size())
+        self.red_team.setStyleSheet("QPushButton{border: 0px solid white; \
+                                        border-radius: 10px;}")
         self.red_team.clicked.connect(self.red_team_clicked)
         
         self.red_team_status = QLabel(self)
-        self.red_team_status.setText("等<br>待<br>中")
-        self.red_team_status.setStyleSheet("QLabel{font-size:" + str(int(self.screen_width/1920 * 40 + self.screen_height/1080 * 40)) + "px; \
-                                            font-weight:bold; \
-                                            font-family:Microsoft YaHei; \
-                                            color: white; }")
-        self.red_team_status.setGeometry(int(self.screen_width * 0.13), int(self.screen_height * 0.3),
-                                        int(self.screen_width * 0.15), int(self.screen_height * 0.5))
+        pixmap = QPixmap("assets/redwaitstatus.png")
+        self.red_team_status.setPixmap(pixmap)
+        self.red_team_status.setScaledContents(True)
+        self.red_team_status.setGeometry(int(self.screen_width * 0.8), int(self.screen_height * 0.1),
+                                        int(self.screen_width * 0.18), int(self.screen_height * 0.6))
         
         self.blue_team = QPushButton(self)
-        self.blue_team.setText("蓝方")
-        self.blue_team.setStyleSheet("  QPushButton{font-size:" + str(int(self.screen_width/1920 * 40 + self.screen_height/1080 * 40)) + "px; \
-                                        font-weight:bold; \
-                                        font-family:Microsoft YaHei; \
-                                        border-radius: 10px; \
-                                        color: blue;}")
-        self.blue_team.setGeometry(int(self.screen_width * 0.77), int(self.screen_height * 0.2),
-                                    int(self.screen_width * 0.15), int(self.screen_height * 0.1))
+        self.blue_team.setGeometry(int(self.screen_width * 0.05), int(self.screen_height * 0.6),
+                                    int(self.screen_width * 0.5), int(self.screen_height * 0.4))
+        pixmap = QPixmap("assets/blueteam.png")
+        icon = QIcon(pixmap)
+        self.blue_team.setIcon(icon)
+        self.blue_team.setIconSize(self.blue_team.size())
+        self.blue_team.setStyleSheet("QPushButton{border: 0px solid white; \
+                                        border-radius: 10px;}")
         
         self.blue_team_status = QLabel(self)
-        self.blue_team_status.setText("等<br>待<br>中")
-        self.blue_team_status.setStyleSheet("QLabel{font-size:" + str(int(self.screen_width/1920 * 40 + self.screen_height/1080 * 40)) + "px; \
-                                            font-weight:bold; \
-                                            font-family:Microsoft YaHei; \
-                                            color: white; }")
-        self.blue_team_status.setGeometry(int(self.screen_width * 0.82), int(self.screen_height * 0.3),
-                                        int(self.screen_width * 0.15), int(self.screen_height * 0.5))
+        pixmap = QPixmap("assets/bluewaitstatus.png")
+        self.blue_team_status.setPixmap(pixmap)
+        self.blue_team_status.setScaledContents(True)
+        self.blue_team_status.setGeometry(int(self.screen_width * 0.02), int(self.screen_height * 0.3),
+                                        int(self.screen_width * 0.18), int(self.screen_height * 0.6))
         self.blue_team.clicked.connect(self.blue_team_clicked)
+        
+        self.reset.raise_()
+        self.start_game.raise_()
         
     def blue_team_clicked(self):
         self.blue_team_flag = 1 - self.blue_team_flag
         if self.blue_team_flag == 1:
-            self.blue_team_status.setText("异<br>常<br>处<br>理<br>中")
+            pixmap = QPixmap("assets/bluewrong.png")
+            self.blue_team_status.setPixmap(pixmap)
         else:
-            self.blue_team_status.setText("顺<br>利<br>进<br>行<br>中")
+            pixmap = QPixmap("assets/bluegaming.png")
+            self.blue_team_status.setPixmap(pixmap)
         
     def red_team_clicked(self):
         self.red_team_flag = 1 - self.red_team_flag
         if self.red_team_flag == 1:
-            self.red_team_status.setText("异<br>常<br>处<br>理<br>中")
+            pixmap = QPixmap("assets/redwrong.png")
+            self.red_team_status.setPixmap(pixmap)
         else:
-            self.red_team_status.setText("顺<br>利<br>进<br>行<br>中")
+            pixmap = QPixmap("assets/redgaming.png")
+            self.red_team_status.setPixmap(pixmap)
         
     def start_game_clicked(self):
-        self.game_status.setText("比赛进行中...")
+        self.fangdai_lock = 1
+        pixmap = QPixmap("assets/gaming.png")
+        self.game_status.setPixmap(pixmap)
         self.main_timer.start(1000)
-        self.red_team_status.setText("顺<br>利<br>进<br>行<br>中")
-        self.blue_team_status.setText("顺<br>利<br>进<br>行<br>中")
+        pixmap = QPixmap("assets/redgaming.png")
+        self.red_team_status.setPixmap(pixmap)
+        pixmap_blue = QPixmap("assets/bluegaming.png")
+        self.blue_team_status.setPixmap(pixmap_blue)
         
     def reset_clicked(self):
+        if self.main_time_left < 420:
+            self.fangdai = self.main_time_left
+        self.fangdai_lock = 0
         self.main_timer.stop()
         self.main_time_left = 420
         self.main_timer_label.setText("07:00")
-        self.game_status.setText("等待比赛中...")
-        self.red_team_status.setText("等<br>待<br>中")
-        self.blue_team_status.setText("等<br>待<br>中")
+        pixmap = QPixmap("assets/waitgame.png")
+        self.game_status.setPixmap(pixmap)
+        pixmap = QPixmap("assets/redwaitstatus.png")
+        self.red_team_status.setPixmap(pixmap)
+        pixmap_blue = QPixmap("assets/bluewaitstatus.png")
+        self.blue_team_status.setPixmap(pixmap_blue)
         
     def update(self):
         self.main_time_left -= 1
@@ -143,9 +152,12 @@ class MainWindow(QMainWindow):
         else:
             self.main_timer.stop()
             self.main_timer_label.setText("00:00")
-            self.red_team_status.setText("等<br>待<br>中")
-            self.blue_team_status.setText("等<br>待<br>中")
-            self.game_status.setText("比赛结束")
+            pixmap = QPixmap("assets/redwaitstatus.png")
+            self.red_team_status.setPixmap(pixmap)
+            pixmap_blue = QPixmap("assets/bluewaitstatus.png")
+            self.blue_team_status.setPixmap(pixmap_blue)
+            pixmap = QPixmap("assets/endgame.png")
+            self.game_status.setPixmap(pixmap)
         
     def __init__(self):
         super().__init__()
@@ -153,8 +165,22 @@ class MainWindow(QMainWindow):
         
     def keyPressEvent(self, envent):
         key = envent.key()
+        modif = envent.modifiers()
         if key == Qt.Key_Escape or key == Qt.Key_Q:
             self.close()
+        if modif == Qt.ControlModifier and key == Qt.Key_Return:
+            self.start_game_clicked()
+        if modif == Qt.ControlModifier and key == Qt.Key_C:
+            self.reset_clicked()
+        if modif == Qt.ControlModifier and key == Qt.Key_R and self.fangdai_lock == 0:
+            self.start_game_clicked()
+            self.fangdai_lock = 1
+            self.main_time_left = self.fangdai
+        if key == Qt.Key_L and self.fangdai_lock == 1:
+            self.red_team_clicked()
+        if key == Qt.Key_A and self.fangdai_lock == 1:
+            self.blue_team_clicked()
+            
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
